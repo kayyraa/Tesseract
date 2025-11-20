6cf901237eb16ee9e41bf8ebdcd030bf378c97a9
kayra <kayrabasbay512@gmail.com>
2025-11-20 18:34:23 +0300

    Cleared all files

--- scripts/hud.js
+++
@@ -1,8 +0,0 @@
-Sidebar.addEventListener("mouseenter", () => {
-    Sidebar.setAttribute("Active", "");
-    document.querySelector(".Chat").setAttribute("Active", "");
-});
-Sidebar.addEventListener("mouseleave", () => {
-    Sidebar.removeAttribute("Active");
-    document.querySelector(".Chat").removeAttribute("Active");
-});
